2196d3d845f7289c7b7691c0dd91c4e57c41d917
melissafilomeno <melissalfilomeno@gmail.com>
2024-12-06 14:59:02 -0500

    init

---
+++ src/main/java/com/myorg/bug_tracker_poc_configserver/BugTrackerPocConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.myorg.bug_tracker_poc_configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerPocConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerPocConfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myorg/bug_tracker_poc_configserver/BugTrackerPocConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorg.bug_tracker_poc_configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerPocConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

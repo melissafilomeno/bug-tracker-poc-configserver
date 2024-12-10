5a51e68c134c2305f01d641e2eb2814f93adb192
melissafilomeno <melissalfilomeno@gmail.com>
2024-12-09 20:49:59 -0500

    configure security

---
+++ src/main/java/com/myorg/bug_tracker_poc_configserver/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.myorg.bug_tracker_poc_configserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+        return http.build();
+    }
+}
